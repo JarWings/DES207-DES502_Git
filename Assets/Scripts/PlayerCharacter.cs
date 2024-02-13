6693d6a8e52db1716ad44eb1c0cae8b8019c15b8
JarWings <fjc13552503296@gmail.com>
2024-02-13 09:47:48 +0000

    move

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -4,15 +4,76 @@ using UnityEngine;
 
 public class PlayerCharacter : MonoBehaviour
 {
-    // Start is called before the first frame update
+    PlayerController controller;
+
+    Rigidbody2D rigid;
+    Animator anim;
+
+    Transform checkGround;
+
+    public float speed = 3.0f;
+    public float jumpspeed = 6.0f;
+
+    bool jump = false;
+    bool isGround = false;
+
     void Start()
     {
-        
+        controller = GetComponent<PlayerController>();
+        rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        checkGround = transform.Find("CheckGround");
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        if (controller.jump)
+        {
+            jump = true;
+        }
+
+        //¸üÐÂ¶¯»­×´Ì¬
+        //update anim state
+        anim.SetBool("IsGround", isGround);
+        anim.SetFloat("Speed", Mathf.Abs(controller.h));
+    }
+
+    private void FixedUpdate()
+    {
+        CheckGround();
+        Move(controller.h);
+        jump = false;
+    }
+
+    private void Move(float h)
+    {
+        Flip(h);
+        float vy = rigid.velocity.y;
+        if (jump && isGround)
+        {
+            anim.SetTrigger("Jump");
+            vy = jumpspeed;
+        }
+        rigid.velocity = new Vector2(h * speed, vy);
+    }
+
+    void CheckGround()
+    {
+        isGround = Physics2D.OverlapCircle(checkGround.position, 0.1f, ~LayerMask.GetMask("Player"));
+    }
+
+    void Flip(float h)
+    {
+        Vector3 scaleLeft = new Vector3(1, 1, 1);
+        Vector3 scaleRight = new Vector3(-1, 1, 1);
+        if (h > 0.1f)
+        {
+            transform.localScale = scaleRight;
+        }
+        else if (h < -0.1f)
+        {
+            transform.localScale = scaleLeft;
+        }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,14 +5,20 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
+    PlayerCharacter player;
+    public float h, v;
+    public bool jump;
+
     void Start()
     {
-        
+        player = GetComponent<PlayerCharacter>();
     }
 
 
     void Update()
     {
-        
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+        jump = Input.GetButtonDown("Jump");
     }
 }
